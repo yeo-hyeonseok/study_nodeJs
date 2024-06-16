64cfc1b6276db05fa9d64d6619e7e51cecffe17d
dugustlr@naver.com <dugustlr@naver.com>
2024-06-16 23:40:49 +0900

    404 에러 페이지 분기 처리하는 법

--- main.js
+++ main.js
@@ -6,43 +6,40 @@ const app = http.createServer((req, res) => {
   const _url = req.url;
   // url의 쿼리스트링 정보 반환
   const queryData = url.parse(_url, true).query;
+  const pathname = url.parse(_url, true).pathname;
+  let title = queryData.id ?? "main";
 
-  let title = queryData.id;
-  if (_url == "/") title = "main";
+  if (pathname === "/") {
+    fs.readFile(`data/${title}.html`, "utf-8", (error, data) => {
+      if (error) throw error;
 
-  if (_url == "/favicon.ico") {
+      const template = `
+      <!doctype html>
+      <html>
+      <head>
+        <title>WEB1 - ${title}</title>
+        <meta charset="utf-8">
+      </head>
+      <body>
+        <h1><a href="/">WEB</a></h1>
+        <ol>
+          <li><a href="/?id=html">HTML</a></li>
+          <li><a href="/?id=css">CSS</a></li>
+          <li><a href="/?id=js">JavaScript</a></li>
+        </ol>
+        <h2>${title}</h2>
+        ${data}
+      </body>
+      </html>
+      `;
+
+      res.writeHead(200);
+      res.end(template);
+    });
+  } else {
     res.writeHead(404);
-    res.end();
-    return;
+    res.end("Not Found");
   }
-
-  res.writeHead(200);
-
-  fs.readFile(`data/${title}.html`, "utf-8", (error, data) => {
-    if (error) throw error;
-
-    const template = `
-    <!doctype html>
-    <html>
-    <head>
-      <title>WEB1 - ${title}</title>
-      <meta charset="utf-8">
-    </head>
-    <body>
-      <h1><a href="/">WEB</a></h1>
-      <ol>
-        <li><a href="/?id=html">HTML</a></li>
-        <li><a href="/?id=css">CSS</a></li>
-        <li><a href="/?id=js">JavaScript</a></li>
-      </ol>
-      <h2>${title}</h2>
-      ${data}
-    </body>
-    </html>
-    `;
-
-    res.end(template);
-  });
 
   /* 
     - 사용자로부터 요청이 들어오면 그에 맞는 페이지를 현재 디렉토리에서 찾아서 응답을 해주는 거임
