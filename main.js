45f6cc1edebafb8c3a8db1d13d08018a0abc948f
dugustlr@naver.com <dugustlr@naver.com>
2024-07-01 14:56:57 +0900

    기초적인 보안 이슈 처리 방법
    - https 통신 방식 사용
    - expressJs 최신 버전 유지
    - helmet 모듈 사용, 적용하는 것만으로 기초적인 보안 이슈로부터 지켜줌 (https://expressjs.com/ko/advanced/best-practice-security.html#use-helmet)
    - nsp로 의존하고 있는 dependency 관리하기

--- main.js
+++ main.js
@@ -2,17 +2,16 @@ const fs = require("fs");
 const express = require("express");
 const bodyParser = require("body-parser");
 const compression = require("compression");
+const helmet = require("helmet");
 const mainRouter = require("./routes");
 const postRouter = require("./routes/post");
 
 const app = express();
 
+app.use(helmet());
 app.use(express.static("public"));
-
 app.use(bodyParser.urlencoded({ extended: false }));
-
 app.use(compression());
-
 app.get("*", (req, _, next) => {
   req.categoryList = fs.readdirSync("data");
   next();
@@ -20,14 +19,12 @@ app.get("*", (req, _, next) => {
 
 /* 라우터 */
 app.use("/", mainRouter);
-
 app.use("/post", postRouter);
 
 /* 에러 처리 */
 app.use((_, res) => {
   res.status(404).send("없는 페이지임");
 });
-
 app.use((err, req, res, next) => {
   console.log(err.stack);
   res.status(500).send("올바르지 않은 요청임");
