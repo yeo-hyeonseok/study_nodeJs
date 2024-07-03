30600e8ef62f8a10eaec7a57fa3b70f3ffd8e5a3
dugustlr@naver.com <dugustlr@naver.com>
2024-07-03 20:37:03 +0900

    에러페이지에 pug 적용
    - pug => html 템플릿의 효율적인 작성을 도와주는 템플릿 엔진임
    
    [+]
    - main.js에 pug 적용
    - index.css에 에러 페이지 관련 스타일 추가
    - view 디렉토리 생성 후, layout.pug와 error.pug 파일 추가

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ const postRouter = require("./routes/post");
 
 const app = express();
 
+app.set("view engine", "pug");
 app.use(helmet());
 app.use(
   csp({
@@ -36,11 +37,15 @@ app.use("/post", postRouter);
 
 /* 에러 처리 */
 app.use((_, res) => {
-  res.status(404).send("없는 페이지임");
+  res
+    .status(404)
+    .render("error", { errorCode: 404, desc: "존재하지 않는 페이지입니다." });
 });
 app.use((err, req, res, next) => {
   console.log(err.stack);
-  res.status(500).send("올바르지 않은 요청임");
+  res
+    .status(500)
+    .render("error", { errorCode: 500, desc: "잘못된 요청입니다." });
 });
 
 app.listen(3000, () => console.log("3000번 포트 연결 중..."));
