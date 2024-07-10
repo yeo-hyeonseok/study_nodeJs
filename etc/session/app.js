e4cd2f4967bfc76493885e1becb1b6ec4c8f5625
dugustlr@naver.com <dugustlr@naver.com>
2024-07-10 22:45:25 +0900

    express-session을 이용한 인증 기능 구현
    - session 객체에 프로퍼티를 추가한 다음 바로 리다이렉션를 하게 되면 session 객체의 변경내용이 반영되기 전에  리다이렉션 되는 경우가 있어 'session.save()'를 사용해야 함
    - 세션을 삭제하고 싶다면 =>session.destroy()
    - 세션 id는 세션이 생성될 때마다 다르게 나타남
    
    [+]
    - '/etc/session' 폴더에 로그인 관련 pug 템플릿 파일 추가

--- etc/session/app.js
+++ etc/session/app.js
@@ -12,6 +12,11 @@ app.set("view engine", "pug");
 app.use(express.static(path.join(__dirname)));
 app.use(
   session({
+    cookie: {
+      secure: false, // https를 통해서만 쿠키가 전송되도록 설정
+      httpOnly: true, // 클라이언트픅 자바스크립트에서 쿠키로 접근할 수 없도록 설정
+      maxAge: 60000, // 만료 시간
+    },
     secret: process.env.SESSION_SECRET, // 세션 id를 서명하는 데 사용되는 비밀 키, 클라이언트가 세션 쿠키를 변조하지 않도록 보호하는 역할을 함, 민감한 정보이므로 환경변수로 설정할 것
     resave: false, // true => 값의 변경 여부 상관없이 계속 저장, false => 값이 변경되는 경우에만 저장
     saveUninitialized: true, // true => 세션이 필요하기 전까지는 구동 x, false => 세션이 필요하던 안 필요하던 무조건 구동(서버 부담 up)
@@ -20,13 +25,37 @@ app.use(
 );
 app.use(bodyParser.urlencoded({ extended: false }));
 
+const authData = {
+  id: "id",
+  pw: "pw",
+};
+
 app.get("/", (req, res) => {
-  if (req.session.num === undefined) {
-    req.session.num = 0;
+  res.render("main", { isAuth: req.session.is_logined });
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.post("/login_process", (req, res) => {
+  const user = req.body;
+
+  if (user.id === authData.id && user.pw === authData.pw) {
+    req.session.is_logined = true;
+    req.session.userId = authData.id;
+    req.session.save(function () {
+      res.redirect("/");
+    });
   } else {
-    req.session.num = req.session.num + 1;
+    res.send("로그인 실패");
   }
-  res.send(`${req.session.num}`);
+});
+
+app.post("/logout_process", (req, res) => {
+  req.session.destroy(function () {
+    res.redirect("/");
+  });
 });
 
 app.listen(4000, () => console.log("4000번 포트 연결 중..."));
