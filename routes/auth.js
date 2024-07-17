38f8f2995bf89f99fd75fe39fa713c403cb26945
dugustlr@naver.com <dugustlr@naver.com>
2024-07-17 19:47:12 +0900

    회원가입 화면 구현
    [+]
    - 메인 화면에 login 화면 링크 추가
    - 'routes/auth.js'에 회원가입 기능 관련 미들웨어 임시 추가

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,14 @@ const express = require("express");
 const router = express.Router();
 
 module.exports = function (passport) {
+  router.get("/register", (req, res) => {
+    res.render("register");
+  });
+
+  router.post("/register_process", (req, res) => {
+    res.send("회원가입 했다치고");
+  });
+
   router.get("/login", (req, res) => {
     const message = req.flash("error");
 
