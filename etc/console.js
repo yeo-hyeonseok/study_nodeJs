c70bc7ecfd796194b4de2159b292bc8e265f0c34
dugustlr@naver.com <dugustlr@naver.com>
2024-08-18 16:14:23 +0900

    필요없는 파일들 삭제
    - 데이터베이스 대용으로 사용하던 'data' 폴더와 폴더 내 파일들 삭제
    -  'etc' 폴더 및 폴더 내의 학습용 파일들 삭제

--- etc/console.js
+++
@@ -1,9 +0,0 @@
-/*
-    - nodeJs에서 console 인자값을 받는 방법
-*/
-const args = process.argv;
-
-// ['내 컴퓨터에서 nodeJs가 설치된 위치', '현재 실행 중인 파일의 절대경로', '인자값1', '인자값2', ...]
-console.log(args);
-
-args[2] == 1 ? console.log("응") : console.log("아니야");
